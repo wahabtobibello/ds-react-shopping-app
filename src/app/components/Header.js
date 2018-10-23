a32cb1073f76835fc950cc2347a9ac7b558222de
Tobi Bello <bello.tobi@outlook.com>
2018-10-24 00:54:49 +0100

    chore: app update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,20 +7,12 @@ const Header = (props) => {
         <header className="shop_header_area carousel_menu_area">
             <div className="carousel_menu_inner">
                 <div className="container">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <nav className="navbar navbar-light bg-light">
                         <Link className="navbar-brand" to="/"><img src="/img/logo.png" alt="" /></Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-
-                        </button>
-
-                        <div className="collapse navbar-collapse flex-row-reverse" id="navbarSupportedContent">
-                            <ul className="navbar-nav justify-content-end">
-                                <style>{`style + li a:before {content:"${cartSize}"!important}`}</style>
-                                <li className="cart_cart"><Link to="/cart"><i className="icon-handbag icons"></i></Link></li>
-                            </ul>
-                        </div>
+                        <ul className="navbar-nav justify-content-end" style={{display:'flex'}}>
+                            <style>{`style + a:before {content:"${cartSize}"!important}`}</style>
+                            <li className="cart_cart"><Link to="/cart"><i className="icon-handbag icons"></i></Link></li>
+                        </ul>
                     </nav>
                 </div>
             </div>
